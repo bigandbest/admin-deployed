cbd63b8135e4d7cbc8a9d0ca0906dfc64e8067b2
Amit <vamit20000@gmail.com>
2026-01-20 23:26:49 +0530

    folder deleted

--- database/add_is_active_to_recommended_store.sql
+++
@@ -1,5 +0,0 @@
--- Add is_active column to recommended_store table
-ALTER TABLE recommended_store ADD COLUMN IF NOT EXISTS is_active BOOLEAN DEFAULT FALSE;
-
--- Ensure only 8 stores can be active (this is enforced in application logic)
--- You can add a trigger or check in app code
